4ab390cb3ce5936fbd101ce5b41ea5072c4ed9d6
estebandalelr <estebandalelr@gmail.com>
2019-12-02 12:31:09 -0500

    Describe two tests for the index page

--- __tests__/index.spec.tsx
+++ __tests__/index.spec.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react'
-import {mount} from 'enzyme'
+import { mount } from 'enzyme'
 import IndexPage from '../pages/index'
 describe('Pages', () => {
-   describe('Index', () => {
-     it('should render without throwing an error', function () {
-       const wrap = mount(<IndexPage/>);
-       expect(wrap.find('h1').text()).toBe('Hello World')
-     })
+  describe('Index', () => {
+    it('should render without throwing an error, and renders the welcome',
+      () => {
+        const wrap = mount(<IndexPage />);
+        expect(wrap.find('h1').text()).toBe('Welcome to Podcaster')
+      })
+  })
+  describe('Index', () => {
+    it('should have a button asking the user to create a Podcast',
+      () => {
+        const wrap = mount(<IndexPage />);
+        expect(wrap.find('button').text()).toBe('Create')
+      })
   })
 })
