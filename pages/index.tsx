23849d02d318a79cfae3606539625675f931a16d
estebandalelr <estebandalelr@gmail.com>
2019-12-02 14:01:48 -0500

    Translation fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const Index = () => {
       <p><span>Podscater</span> helps you plan your script, from your research to recording.</p>
       <ul>
         <li>Add the links you used on your research</li>
-        <li>Add your invitees social networks</li>
+        <li>Add your guests social networks</li>
         <li>Add any sponsors you have</li>
         <li>Write your script, and know how long it takes to read</li>
       </ul>
