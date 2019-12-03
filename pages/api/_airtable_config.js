bd298af28e4002d0b81782202069721fd18a406f
estebandalelr <estebandalelr@gmail.com>
2019-12-03 16:22:59 -0500

    Setup backend serverless functions

---
+++ pages/api/_airtable_config.js
@@ -0,0 +1,7 @@
+var Airtable = require('airtable');
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: process.env.AIRTABLE_API_KEY
+});
+var base = Airtable.base(process.env.AIRTABLE_BASE_ID);
+export default base;
---
+++ pages/api/postPodcast.js
@@ -0,0 +1,21 @@
+import base from './_airtable_config'
+import { throws } from 'assert'
+export default (req, res) => {
+   const table = base('Podcasts')
+  console.log(req.body)
+  try {
+    table.create([req.body], function(err, records) {
+      if (err) {
+        console.log(err)
+        throws(err);
+        return;
+      }
+      res.send(records)
+    });
+  } catch (error) {
+    res.setHeader('Content-Type', 'application/json')
+    res.statusCode = 400
+    console.log(error)
+    throws(error);
+  }
+}
