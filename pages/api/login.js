91f26e38ae4c8e153754533e94d027c55e8dc894
estebandalelr <estebandalelr@gmail.com>
2019-12-03 21:30:40 -0500

    Added api to login

---
+++ pages/api/login.js
@@ -0,0 +1,44 @@
+import base from './_airtable_config'
+import { throws } from 'assert'
+import jwt from 'jsonwebtoken'
+
+export default (req, res) => {
+  const table = base('Users')
+  console.log(req.body)
+  try {
+    table.select(
+      {
+        maxRecords: 1,
+        filterByFormula: "(" + "Email" + "='" + req.body.email.toString() + "')",
+        fields: ["Email", "Password"]
+      }
+    ).eachPage(
+      function page(records, fetchNextPage) {
+        if (records.length === 0) {
+          res.send({ error: "User not found" })
+        }
+        if (records[0].fields.Password === req.body.password) {
+
+          let token = jwt.sign(
+            {
+              email: records[0].fields.Email,
+              id: records[0].id
+            },
+            process.env.JWT_SIGNING_SECRET
+          )
+          res.send({ podcasterUserJWT: token })
+        } else {
+          res.send({ error: "Wrong password" })
+        }
+        fetchNextPage();
+      }, function done(err) {
+        if (err) { console.error("error updating" + err); reject(err); return false; }
+      }
+    );
+  } catch (error) {
+    res.setHeader('Content-Type', 'application/json')
+    res.statusCode = 400
+    console.log(error)
+    throws(error);
+  }
+}
