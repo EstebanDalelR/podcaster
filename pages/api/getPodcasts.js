1d50ad8c7efd73b4aac89f93c97fbb93736b5ce8
estebandalelr <estebandalelr@gmail.com>
2019-12-04 21:42:05 -0500

    Only send relevant fields

--- pages/api/getPodcasts.js
+++ pages/api/getPodcasts.js
@@ -9,28 +9,36 @@ export default (req, res) => {
   jwt.verify(req.body.jwt, process.env.JWT_SIGNING_SECRET, (err, decoded) => {
     decodedJWT = decoded
   });
-  let foundRecords =[]
+  let foundRecords = []
   try {
     let filterFormula = `{UserEmail}="${decodedJWT.email}"`
     table.select({
-      filterByFormula: filterFormula
+      filterByFormula: filterFormula,
+      fields: [
+        "Title",
+        "Links",
+        "Summary",
+        "Guests",
+        "Sponsors",
+        "Script",
+      ]
     }).eachPage(function page(records, fetchNextPage) {
       // This function (`page`) will get called for each page of records.
-      records.forEach(function(record) {
-          foundRecords.push({
-            id: record.id,
-            fields: record.fields
-          })
+      records.forEach(function (record) {
+        foundRecords.push({
+          id: record.id,
+          fields: record.fields
+        })
       });
       // To fetch the next page of records, call `fetchNextPage`.
       // If there are more records, `page` will get called again.
       // If there are no more records, `done` will get called.
       fetchNextPage();
-  
-  }, function done(err) {
+
+    }, function done(err) {
       if (err) { console.error(err); return; }
       res.send(foundRecords)
-  })
+    })
   } catch (error) {
     res.setHeader('Content-Type', 'application/json')
     res.statusCode = 400
