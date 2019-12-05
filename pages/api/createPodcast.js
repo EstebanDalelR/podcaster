4974a0ffd5a6c58338cb4c19a32bb4d0d4f83588
estebandalelr <estebandalelr@gmail.com>
2019-12-04 19:42:35 -0500

    Fix so it saves the user that created a podcast

--- pages/api/createPodcast.js
+++ pages/api/createPodcast.js
@@ -1,13 +1,26 @@
 import base from './_airtable_config'
 import { throws } from 'assert'
+import jwt from 'jsonwebtoken'
+
 export default (req, res) => {
-   const table = base('Podcasts')
+  const table = base('Podcasts')
   console.log(req.body)
+  let decodedJWT
+  jwt.verify(req.body.jwt, process.env.JWT_SIGNING_SECRET, (err, decoded) => {
+    decodedJWT = decoded
+  });
+  let sendObject = {
+    "fields": {
+      ...req.body.fields,
+      "User": [`${decodedJWT.id}`
+      ]
+    }
+  }
   try {
-    table.create([req.body], function(err, records) {
+    table.create([sendObject], function (err, records) {
       if (err) {
         console.log(err)
-        throws(err);
+        /* throws(err); */
         return;
       }
       res.send(records)
@@ -16,6 +29,6 @@ export default (req, res) => {
     res.setHeader('Content-Type', 'application/json')
     res.statusCode = 400
     console.log(error)
-    throws(error);
-  }
+/*     throws(error);
+ */  }
 }
