a4c2901258f76c8684ad40924589502a4a102d18
estebandalelr <estebandalelr@gmail.com>
2019-12-06 10:40:29 -0500

    Added section to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,28 +30,46 @@ const Index = () => {
           grid-template-columns: repeat(2, 1fr);
           grid-template-rows: repeat(2, 1fr);
         }
+        .callToAction{
+          display: flex;
+          align-items: center;
+          flex-direction: column;
+          text-align: center;
+        }
+        button{
+          background-color: #3549ff;
+          color: white;
+          border: none;
+          padding-inline: 2%;
+          padding-block: 1em;
+          border-radius: 4px;
+        }
       `}</style>
       <h1>Welcome to Podcaster</h1>
       <p>
         Congrats on deciding to share your knowledge and opinions to the world.
         We know a podcast might seem like a lot, from editing to publishing, but we're here to help.
       </p>
-      <h2>How it works</h2>
-      <p><span>Podscater</span> helps you plan your script, from your research to recording.</p>
-      <div className="benefitsHolder">
-        {benefits.map((benefit, index) =>
-          <BenefitsCard text={benefit.text} icon={benefit.icon} key={index}/>
-        )}
-      </div>
-      <h2>Start now</h2>
-      {userJWT
-        ? <Link href="/create">
-          <button>Create Podcast</button>
-        </Link>
-        : <Link href="/signup">
-          <button>Create Account</button>
-        </Link>
-      }
+      <section>
+        <h2>How it works</h2>
+        <p><span>Podscater</span> helps you plan your script, from your research to recording.</p>
+        <div className="benefitsHolder">
+          {benefits.map((benefit, index) =>
+            <BenefitsCard text={benefit.text} icon={benefit.icon} key={index} />
+          )}
+        </div>
+      </section>
+      <section className="callToAction">
+        <h2>Start now</h2>
+        {userJWT
+          ? <Link href="/create">
+            <button>Create Podcast</button>
+          </Link>
+          : <Link href="/signup">
+            <button>Create Account</button>
+          </Link>
+        }
+      </section>
     </>
   )
 }
