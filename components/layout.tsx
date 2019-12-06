e7364f9dcd52a64d294c308a8ff8cbeefa1126cb
estebandalelr <estebandalelr@gmail.com>
2019-12-06 10:39:55 -0500

    Added footer

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,12 @@
 export default function Layout(props) {
   const { children } = props
+  let social = {
+    li: "https://www.linkedin.com/in/estebandalelr/",
+    gh: "https://github.com/estebandalelr",
+    tw: "https://twitter.com/estebandalelr",
+    fb: "https://facebook.com/estebandalelr" 
+  }
+  let socialKeys= Object.keys(social)
   return (
     <div className="background">
       <style jsx>{`
@@ -10,14 +17,47 @@ export default function Layout(props) {
           width: 100%;
         }
         .container{
-          padding-left: 2%;
-          padding-right: 2%;
-          padding-top: 2%;
+          padding: 2%;
         }
-      `}</style>
+        footer{
+          width: 100%;
+          background-color: #70ffe7;
+          align-items: center;
+          color: darkgrey;
+          display: flex;
+          justify-content: space-between;
+        }
+        footer > p{
+          margin: 0;
+          width: 50%;
+          text-shadow: none;
+        }
+        img{
+          width: 2em;
+          height: 2em;
+          margin: 0.5em;
+        }
+        .social{
+          width: 50%;
+          display: flex;
+          align-items: center;
+          justify-content: flex-end;
+        }
+        `}</style>
       <div className="container">
         {children}
       </div>
+      <footer>
+        <p>Created by Esteban Dalel R</p>
+        <div className="social">
+          {socialKeys.map((key, index)=> 
+          <div key={index}>
+            <a href={social[key]}>
+              <img src={`/icons/${key}.svg`} alt="social icon"/>
+            </a>
+          </div>)}
+        </div>
+      </footer>
     </div>
   )
 }
