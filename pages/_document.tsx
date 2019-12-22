da5fdc53ed14940390b255b06633acb760841bff
estebandalelr <estebandalelr@gmail.com>
2019-12-22 12:05:07 -0500

    Modify global doc

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,34 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components'
 
-export default function MyDocument() {
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+}
+/* function MyDocument() {
+
   return (
     <Html>
       <Head>
@@ -35,3 +63,8 @@ export default function MyDocument() {
     </Html>
   )
 }
+MyDocument.getInitialProps= async(ctx)=> {
+  const initialProps = await Document.getInitialProps(ctx)
+  return { ...initialProps }
+}
+export default MyDocument */
